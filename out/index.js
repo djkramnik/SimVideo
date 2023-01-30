da500b3a5ee55180d018f1b94ae2cd9ca868868f
David Gurr <davidk.gurr@gmail.com>
2023-01-29 20:18:43 -0500

    video file selector

--- out/index.js
+++ out/index.js
@@ -1,1 +1,19 @@
 // https://github.com/ffmpegwasm/ffmpeg.wasm
+
+// handlers 
+
+const fileUpload = document.getElementById('file-upload')
+const video = document.getElementById('video')
+
+fileUpload.addEventListener('change', loadSelectedVideo, false)
+
+function loadSelectedVideo(event) {
+  const file = event.target.files[0]
+  if (!video.canPlayType(file.type)) {
+    window.alert('Cannot play this file')
+    return
+  }
+  const fileURL = URL.createObjectURL(file)
+  video.src = fileURL
+}
+
